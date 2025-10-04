fab82c356384a737ce76cee987231f951cc21bce
Vinicius de Oliveira <ti.vinicius.dev@gmail.com>
2025-10-04 00:10:18 -0300

    Allow disable fields

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,15 @@
             v-model="form[field].value" 
             :id="field" 
             :name="field" 
+            :class="{ 'inactive': !form[field].active }"
             type="range"
             min="0" 
             max="10" 
           />
-          <strong>{{ form[field].value }}</strong>
+          <div class="input-visibility-container">
+            <strong>{{ form[field].value }}</strong>
+            <input type="checkbox" v-model="form[field].active">
+          </div>
         </div>
       </form>
       <button @click="showGreenProbability = !showGreenProbability">
@@ -39,35 +43,43 @@ export default {
       form: {
         lastMatchesHomeGreen: {
           value: 0,
-          label: "Em quantos dos últimos 10 jogos do mandante a aposta seria ganha?"
+          label: "Em quantos dos últimos 10 jogos do mandante a aposta seria ganha?",
+          active: true
         },
         lastMatchesAwayGreen: {
           value: 0,
-          label: "Em quantos dos últimos 10 jogos do visitante a aposta seria ganha?"
+          label: "Em quantos dos últimos 10 jogos do visitante a aposta seria ganha?",
+          active: true
         },
         matchBetweenBoth: {
           value: 5,
-          label: "Proporção de acertos nos últimos 3-5 confrontos diretos. (Nota 0-10. Se não houver dados, usar nota neutra 5.0)"
+          label: "Proporção de acertos nos últimos 3-5 confrontos diretos. (Nota 0-10. Se não houver dados, usar nota neutra 5.0)",
+          active: true
         },
         myPerformanceHome: {
           value: 5,
-          label: "Minha % de acerto em apostas no mandante"
+          label: "Minha % de acerto em apostas no mandante",
+          active: false
         },
         myPerformanceAway: {
           value: 5,
-          label: "Minha % de acerto em apostas no visitante"
+          label: "Minha % de acerto em apostas no visitante",
+          active: false
         },
         myPerformanceLeague: {
           value: 5,
-          label: "Minha % de acerto em apostas na competição"
+          label: "Minha % de acerto em apostas na competição",
+          active: false
         },
         teamsMoment: {
           value: 0,
-          label: "O momento das equipes está propício para essa entrada se concretizar?"
+          label: "O momento das equipes está propício para essa entrada se concretizar?",
+          active: true
         },
         teamsInterest: {
           value: 0,
-          label: "O interesse das equipes na partida está condizente com a entrada?"
+          label: "O interesse das equipes na partida está condizente com a entrada?",
+          active: true
         }
       },
       showGreenProbability: false
@@ -75,7 +87,9 @@ export default {
   },
   computed: {
     greenProbability() {
-      const values = Object.values(this.form).map(item => Number(item.value));
+      const values = Object.values(this.form)
+        .filter(item => item.active)
+        .map(item => Number(item.value));
       return (values.length > 0 ? values.reduce((sum, v) => sum + v, 0) / values.length : 0).toFixed(2);
     }
   }
@@ -111,13 +125,25 @@ main {
       flex-direction: column;
       align-items: center;
       justify-content: center;
+      text-align: center;
 
-      input {
+      input[type="range"] {
         width: 30rem;
-      }
 
-      strong {
-        color: aquamarine;
+        &.inactive {
+          pointer-events: none;
+          opacity: .5;
+        }
+      }
+      .input-visibility-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: .6rem;
+        
+        strong {
+          color: aquamarine;
+        }
       }
     }
   }
